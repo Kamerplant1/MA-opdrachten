ed0a6e9e28bd9debfce89f61798e4ca6aed42bbe
Tristan Wals <39040@ma-web.nl>
2024-09-25 17:29:25 +0200

    Update welke_dag.js

--- welke_dag.js
+++ welke_dag.js
@@ -3,6 +3,6 @@ const huidigeDag = "dag 1";
 
 if(huidigeDag == "dag 1" || huidigeDag == "dag 2" || huidigeDag == "dag 3" || huidigeDag == "dag 4" || huidigeDag == "dag 5") {
     console.log("Ik moet naar school");
-} if(huidigeDag == "dag 6" || huidigeDag == "dag 7") {
+} if(huidigeDag == "dag 6" || huidigeDag == "dag777 7") {
     console.log("Ik kan uitslapen");
 }
