22028be1a17445c600abfea5be16600b82e5b260
Kamerplant1 <Walstristan@gmail.com>
2024-10-02 22:32:45 +0200

    Update wiskunde.js

--- wiskunde.js
+++ wiskunde.js
@@ -1,12 +1,27 @@
-function langzijdedriehoek(a,b){
+function kubus(lengte, hoogte, diepte){
+    let kubusInhoud = lengte * hoogte * diepte;
+    return kubusInhoud;
+}
+console.log (kubus(4,3,2));
+
+function cilinder(radius, hoogte){
+    let kwadraat = Math.pow(radius, 2);
+    let cilinderInhoud = Math.PI * kwadraat * hoogte
+    return cilinderInhoud;
+}
+console.log (cilinder(8,20));
+
+function rechtehoek(a, b){
     let aKwadraat = Math.pow(a,2);
     let bKwadraat = Math.pow(b,2);
     let cKwadraat = aKwadraat + bKwadraat;
     let wortel = Math.sqrt(cKwadraat);
-    return wortel
-}console.log(langzijdedriehoek(6,6))
+    return wortel;
+}
+console.log (rechtehoek(8,8));
 
-function gemiddelde(num1,num2,num3,num4,num5,num6,num7){
-    let som = num1+num2+num3+num4+num5+num6+num7;
+function getallen7(getal1, getal2, getal3, getal4, getal5, getal6, getal7){
+    let som = getal1 + getal2 + getal3 + getal4 + getal5 + getal6 + getal7;
     return som / 7;
-}console.log(gemiddelde(1,2,3,4,5,6,7))
+}
+console.log (getallen7(1,2,3,4,5,6,7))
